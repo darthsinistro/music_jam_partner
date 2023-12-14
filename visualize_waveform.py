8096ee5c4ee16e30028e0249416f193bc5715fd8
Rahul <darthsinistro@gmail.com>
2023-12-13 23:17:52 -0600

    Able to input and visualize the waveform. Now for the STFT with microphone.

--- visualize_waveform.py
+++ visualize_waveform.py
@@ -3,16 +3,16 @@ import struct
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-from tkinter import TclError
 
 # constants
 CHUNK = 1024 * 2             # samples per frame
 FORMAT = pyaudio.paInt16     # audio format (bytes per sample?)
 CHANNELS = 1                 # single channel for microphone
 RATE = 44100                 # samples per second
+INPUT_DEVICE_ID = 2          # 2 for the laptop mic
 
 # create matplotlib figure and axes
-fig, ax = plt.subplots(1, figsize=(15, 7))
+fig, ax = plt.subplots(figsize=(15, 7))
 
 # pyaudio class instance
 p = pyaudio.PyAudio()
@@ -23,58 +23,28 @@ stream = p.open(
     channels=CHANNELS,
     rate=RATE,
     input=True,
+    input_device_index=INPUT_DEVICE_ID,
     output=True,
-    frames_per_buffer=CHUNK,
-    input_device_index=1
+    frames_per_buffer=CHUNK
 )
 
 # variable for plotting
-x = np.arange(0, 2 * CHUNK, 2)
+x = np.arange(0, CHUNK)
 
 # create a line object with random data
-line, = ax.plot(x, np.random.rand(CHUNK), '-', lw=2)
-
-# basic formatting for the axes
-ax.set_title('AUDIO WAVEFORM')
-ax.set_xlabel('samples')
-ax.set_ylabel('volume')
-ax.set_ylim(0, 255)
-ax.set_xlim(0, 2 * CHUNK)
-plt.setp(ax, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
+line, = ax.plot(x, x, '-', lw=2)
+ax.set_ylim(-10000,10000)
 
 # show the plot
 plt.show(block=False)
 
-print('stream started')
-
-# for measuring frame rate
-frame_count = 0
-start_time = time.time()
-
 while True:
-    
     # binary data
-    data = stream.read(CHUNK)  
+    data = stream.read(CHUNK)
     
     # convert data to integers, make np array, then offset it by 127
-    data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
-    
-    # create np array and offset by 128
-    data_np = np.array(data_int, dtype='b')[::2] + 128
+    data_int = struct.unpack(str(CHUNK) + 'h', data)
+    data_np = np.array(data_int)
     
     line.set_ydata(data_np)
-    
-    # update figure canvas
-    try:
-        fig.canvas.draw()
-        fig.canvas.flush_events()
-        frame_count += 1
-        
-    except TclError:
-        
-        # calculate average frame rate
-        frame_rate = frame_count / (time.time() - start_time)
-        
-        print('stream stopped')
-        print('average frame rate = {:.0f} FPS'.format(frame_rate))
-        break
+    plt.pause(0.001)
